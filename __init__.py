4e7e9b058ca951f31eb256a98ce9b11119febbc1
Eric <ericalcaide1@gmail.com>
2017-10-28 18:15:21 +0200

    deploying port

--- __init__.py
+++ __init__.py
@@ -40,4 +40,8 @@ def record(name, link):
 		
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # Deploying
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
+	# Debugging
+	# app.run(debug=True, host='0.0.0.0')
